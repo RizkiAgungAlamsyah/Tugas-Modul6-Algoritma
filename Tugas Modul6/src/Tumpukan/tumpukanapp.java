d436b7efc6eea3b6537accc42a0ab299bd7c78f9
RizkiAgungAlamsyah <80383706+RizkiAgungAlamsyah@users.noreply.github.com>
2021-05-30 18:44:05 +0700

    Add files via upload

---
+++ Tugas Modul6/src/Tumpukan/tumpukanapp.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tumpukan;
+
+/**
+ *
+ * @author agung
+ */
+public class tumpukanapp {
+    public static void main(String[] args){
+        tumpukan tumpukan=new tumpukan(10);
+        tumpukan.push(50);
+        tumpukan.baca();
+        System.out.println(" ");
+        tumpukan.push(100);
+        tumpukan.baca();
+        System.out.println(" ");
+        tumpukan.pop();
+        tumpukan.baca();
+        System.out.println(" ");
+        tumpukan.push(60);
+        tumpukan.baca();
+        System.out.println(" ");
+        tumpukan.push(80);
+        tumpukan.baca();
+        long nilai2=tumpukan.pop();
+        
+        System.out.println(" ");
+        tumpukan.baca();
+        System.out.println(" ");
+        long nilai3=tumpukan.peek();
+        System.out.println("nilai top  = " +nilai3);
+        System.out.println("Rizki Agung Alamsyah");
+        
+      
+    }
+    
+}
